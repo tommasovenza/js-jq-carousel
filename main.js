59198a0a17d1f24e9e2c5fffe8f63e0e03ac994f
Tommaso Venza <tommasovenza@gmail.com>
2020-06-09 16:03:06 +0200

    riepilogo le idee e imposto click di prova

---
+++ main.js
@@ -0,0 +1,23 @@
+// Analizzate il codice html e css prima di iniziare
+
+// Scrivete nei commenti gli step che vi servono per raggiungere l’obbiettivo
+// Andate avanti passo passo testando ogni singolo pezzo gradualmente
+// Fate I miglioramenti grafici solo dopo aver completato la parte js
+// Attenzione a dove dichiarate le variabili che contengono gli elementi html, lo scopo è importante
+
+
+// imposto il document ready
+
+// vado a leggere la documentazione di jQuery per ricordare le funzioni next() e prev()
+
+$(document).ready(
+    function() {
+
+        // imposto il click
+        $('.next').click(
+            function() {
+                alert('test');
+            }
+        )
+    }
+);
